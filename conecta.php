8b9236e4830dbfe8203c926b7b20746e91796ca4
caioresendede <caioresende14@gmail.com>
2024-08-01 20:10:43 -0300

    consulta e conexao banco de dados

---
+++ conecta.php
@@ -0,0 +1,9 @@
+<?php 
+
+$conexao = mysqli_connect("localhost:3307","root","","sistema");
+if (!$conexao) {
+    die ("Falhou! ERRO: " . mysqli_connect_error());
+} echo "Conectado com sucesso!";
+$mysqli_
+
+?>
